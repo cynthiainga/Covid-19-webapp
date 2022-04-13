dc3f0582dad373c823b5400f7b389d809a355a2f
Cynthia Inga <candyinga67@gmail.com>
2022-04-13 12:24:19 +0200

    Add the header of the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,31 @@
+import { Link, useLocation } from 'react-router-dom';
+import { TiArrowBackOutline } from 'react-icons/ti';
+import { AiOutlineSetting as Settings } from 'react-icons/ai';
+import { FaMicrophone as Mic } from 'react-icons/fa';
+import logo from '../images/logo.jpg';
+
 const Header = () => {
-    return (  );
-}
+  const Backdrop = <TiArrowBackOutline />;
+  const location = useLocation();
+  const Backpath = location.pathname.includes('country') || location.pathname.includes('continent') ? Backdrop : '';
+
+  return (
+
+    <nav className="navBar">
+      <span>
+        <img src={logo} alt="logo" className="logo" />
+        <h1> 2022 Covid-Tracker</h1>
+        <div>
+          <Mic />
+          <Settings />
+        </div>
+      </span>
+      <div className="backpath">
+        <Link to={{ pathname: '/' }}>{Backpath}</Link>
+        <h1>Tracked Data</h1>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
